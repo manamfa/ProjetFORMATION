0ce080e57cf0cee561c65b9556ceed53e11daca6
manamafashatheogene@gmail.com <manamfashatheogene@gmail.com>
2022-01-07 17:52:44 +0200

    ajout d'un index pour affichage des clients

--- CRDLPROJET/Controllers/clientsController.cs
+++ CRDLPROJET/Controllers/clientsController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Net;
+using System.Net; 
 using System.Web;
 using System.Web.Mvc;
 using CRDLPROJET.Models;
