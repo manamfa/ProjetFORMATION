3f9849b74055fb31799f61756ff40723c27b132e
manamafashatheogene@gmail.com <manamfashatheogene@gmail.com>
2022-01-07 17:55:40 +0200

    ajout d'un craete1 fonction pour ajoute des clients taches d'utilisateur

--- CRDLPROJET/Controllers/clientsController.cs
+++ CRDLPROJET/Controllers/clientsController.cs
@@ -69,6 +69,7 @@ namespace CRDLPROJET.Controllers
         }
         public ActionResult Create1()
         {
+
             var client = db.clients;
             ViewData["client"] = client.ToList();
 
