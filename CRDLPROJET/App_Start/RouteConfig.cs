8afcc1c6b8cb5a5cfc10963b4a146c3c7025bdfb
Theogene <manamfashatheogene@gmail.com>
2022-01-07 11:32:23 +0200

    appel de premiere page pour s'authentifier

--- CRDLPROJET/App_Start/RouteConfig.cs
+++ CRDLPROJET/App_Start/RouteConfig.cs
@@ -13,9 +13,11 @@ namespace CRDLPROJET
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
+
                 defaults: new { controller ="users"/*"historiquefacturations"*/, action =/*"Login"*/"Login", id = UrlParameter.Optional }
             );
         }
