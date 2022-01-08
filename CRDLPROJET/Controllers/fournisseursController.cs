85e3f4b02c287c3bc862280d18217699008755b7
manamafashatheogene@gmail.com <manamfashatheogene@gmail.com>
2022-01-08 09:16:05 +0200

    ajout d'une fonction Edit pour Modifier le Fournisseur

--- CRDLPROJET/Controllers/fournisseursController.cs
+++ CRDLPROJET/Controllers/fournisseursController.cs
@@ -42,6 +42,7 @@ namespace CRDLPROJET.Controllers
             return View();
         }
 
+
         // POST: fournisseurs/Create
         // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
         // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
@@ -95,7 +96,6 @@ namespace CRDLPROJET.Controllers
             }
             return View(fournisseur);
         }
-
         // GET: fournisseurs/Delete/5
         public ActionResult Delete(int? id)
         {
